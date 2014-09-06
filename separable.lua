e23b1555857db078f0efe3da759d70cc5a094f09
Steven Johnson <steve@xibalbastudios.com>
2014-09-05 20:10:37 -0500

    Minor separable advancements

--- separable.lua
+++ separable.lua
@@ -24,22 +24,93 @@
 --
 
 -- Standard library imports --
+local max = math.max
+local min = math.min
 
 -- Modules --
+local linear_convolution = require("signal_ops.linear_convolution")
 local svd = require("linear_algebra_ops.svd")
+
+-- Imports --
+local Convolve_2D = linear_convolution.Convolve_2D
 
 -- Exports --
 local M = {}
 
+-- --
+local Opts1, Opts2 = { into = {}, shape = "same" }, { shape = "same" }
+
+-- --
+local ToAdd = {}
+
 --- DOCME
-function M.DecomposeKernel (kernel, kcols, kn)
-	kn = kn or #kernel
+function M.Convolve_2D (signal, kernel, scols, opts)
+	local csignal, max_rank = opts and opts.into or {}, opts and opts.max_rank or kernel.max_rank
+
+	if max_rank > 0 then
+		Opts2.into = csignal
+
+		--
+		local n, u, v = kernel.n, kernel.uarr, kernel.varr
+
+		Convolve_2D(Convolve_2D(signal, u[1], scols, 1, Opts1), v[1], scols, n, Opts2)
+
+		Opts2.into = ToAdd
+
+		for i = 2, min(max_rank, kernel.max_rank) do
+			--
+		end
+	else
+		for i = 1, #signal do
+			csignal[i] = 0
+		end
+	end
+
+	return csignal
+end
+
+--
+local function FindRank (s, n)
+	for i = 1, n do
+		if s[i] < 1e-6 then
+			return i - 1
+		end
+	end
+
+	return n
+end
+
+--- DOCME
+function M.DecomposeKernel (kernel, kcols, opts)
+	local kn = opts and opts.kn or #kernel
+	local decomp = opts and opts.into or {}
+	local uarr, varr = decomp.uarr or {}, decomp.varr or {}
 
 	if kcols^2 == kn then
 		local u, s, v = svd.SVD_Square(kernel, kcols)
+
+		for i = 1, kcols do
+			local uv, vv, j = uarr[i] or {}, varr[i] or {}, 1
+
+			for ci = i, kn, kcols do
+				uv[j], vv[j], j = u[ci], v[ci], j + 1
+			end
+
+			for k = max(#uv, #vv), j, -1 do
+				uv[k], vv[k] = nil
+			end
+
+			uarr[i], varr[i] = uv, vv
+		end
+
+		decomp.max_rank, decomp.n = FindRank(s, kcols), kcols
 	else
 		-- ...
 	end
+
+	decomp.uarr, decomp.varr = uarr, varr
+
+	return decomp
 end
 
 --[=[
