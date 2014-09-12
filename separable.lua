d7d88204e8a2595e1641d1fef225b94f7d97bf1b
Steven Johnson <steve@xibalbastudios.com>
2014-09-11 19:02:08 -0500

    Further along with FFT approach

--- separable.lua
+++ separable.lua
@@ -72,45 +72,42 @@ end
 local ToAdd = {}
 
 -- --
-local B = {}
+local B, C, D = {}, {}, {}
 
 -- --
-local PrecomputedKernelFunc = utils.MakePrecomputedKernelFunc_1D(B)
+local PrecomputedKernelFunc = utils.MakePrecomputedKernelFunc_1D(C)
 
 --
-local function FFT (max_rank, u, v)
-	-- In signal, do same for rows on startup...
-	local lhs,ss = {}, {}
-	for i = 1, #u, nu do
-		local out = {}
-		for j = 0, nu - 1 do
-			ss[j + 1] = mat[i + j]
-		end
-		utils.PrecomputeKernel_1D(out, up, ss, nu)
-		lhs[#lhs + 1] = out
-	end
+local function AuxConvolve_FFT (max_rank, u, v, up, vp, size)
+
 	-- Then do just multiply / IFFT on left
 	local sss,ttt,uuu={},{},{}
 
-	local ulen, up, vp = u.n, nil
-	local halfup, halfvp = .5 * up, .5 * vp
+	local halfup, halfvp, ulen, count = .5 * up, .5 * vp, u.n, C.n
 
 	for rank = 1, max_rank do
-	--	local u,v=arr1[rank],arr2[rank]
-		for i = 1, #lhs do
-			fft_utils.Multiply_1D(lhs[i], u, up, sss)
+		local index = 1
+
+		for i = 1, size, count do
+			for j = 0, count - 1 do
+				C[j + 1] = Columns[i + j]
+			end
+
+			fft_utils.Multiply_1D(C, u, up, B)
 
 			-- ...transform back to the time domain...
-			real_fft.RealIFFT_1D(sss, halfup)
+			real_fft.RealIFFT_1D(B, halfup)
 
 			-- ...and get the requested part of the result.
---			for i = 1, ulen do
---				uuu[i] = sss[i] <- columns...
---			end
+			for i = 1, ulen do
+				D[index], index = B[i], index + 1
+			end
+		end
+--- ^^^ Some of those indices are surely off!
+		for i = 1, 20 do -- err, whatever this is :)
+			PrecomputedKernelFunc(vp, B, ulen, v)
 
-			PrecomputedKernelFunc(vp, sss, ulen, v)
-
-			real_fft.RealIFFT_1D(B, halfvp)
+			real_fft.RealIFFT_1D(C, halfvp)
 --[[
 Convolve_1D(signal, kernel, opts)
 		end
@@ -118,24 +115,42 @@ Convolve_1D(signal, kernel, opts)
 		count, from, size, offset, signal, opts, kernel = len, Columns, offset, 0, RowVector, RowOpts, v
 ]]
 		end
+
+		if rank > 1 then
+			-- switch target to ToAdd...
+		else
+			-- accumulate ToAdd...
+		end
 	end
 end
 
 --- DOCME
 function M.Convolve_2D (signal, scols, decomp, opts)
 	local csignal, sn, p = opts and opts.into or {}, opts and opts.sn or #signal, decomp.p
-	local srows, len = sn / scols, scols + p - 1
+	local max_rank, srows, len = opts and opts.max_rank or decomp.max_rank, sn / scols, scols + p - 1
 	local n = len * (srows + decomp.q - 1)
 
 	-- Typical case: matrix was successfully decomposed.
-	local max_rank = min(opts and opts.max_rank or decomp.max_rank, decomp.max_rank)
-
 	if max_rank > 0 then
 		local u, v = decomp.uarr, decomp.varr
 
 		--
-		if u[1].n then
-			-- FFT!
+		if decomp.up then
+			local size = 0
+
+			for i = 1, #signal, scols do
+				for j = 0, scols - 1 do
+					B[j + 1] = signal[i + j]
+				end
+
+				utils.PrecomputeKernel_1D(C, decomp.up, B, scols)
+
+				for j = 1, C.n do
+					Columns[size + 1], size = C[j], size + 1
+				end
+			end
+
+			AuxConvolve_FFT(max_rank, u, v, decomp.up, decomp.vp, size)
 		else
 			-- Trim work vectors as necessary.
 			for i = #ColVector, srows + 1, -1 do
@@ -150,7 +165,7 @@ function M.Convolve_2D (signal, scols, decomp, opts)
 			-- any case, at this point the output signal and accumulator are the same thing.
 			RowOpts.into = csignal
 
-			for rank = 1, max_rank do
+			for rank = 1, min(max_rank, decomp.max_rank) do
 				AuxConvolve(signal, scols, sn, u[rank], v[rank], len)
 
 				-- If the matrix was non-separable, refine the approximation with as many convolutions as
@@ -195,16 +210,18 @@ function M.DecomposeKernel (kernel, kcols, opts)
 
 	assert(not scols == not srows, "Missing signal columns or row count")
 
+	local decomp = opts and opts.into or {}
+	local uarr, varr = decomp.uarr or {}, decomp.varr or {}
+
 	--
 	if scols then
 		TempU, ulen, up = TempU or {}, utils.LenPower(scols, kcols)
 		TempV, vlen, vp = TempV or {}, utils.LenPower(srows, kn / kcols)
+
+		decomp.up, decomp.vp = up, vp
 	end
 
 	--
-	local decomp = opts and opts.into or {}
-	local uarr, varr = decomp.uarr or {}, decomp.varr or {}
-
 	if kcols^2 == kn then
 		local u, s, v = svd.SVD_Square(kernel, kcols)
 
